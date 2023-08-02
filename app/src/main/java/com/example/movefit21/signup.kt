a2e02703b49f909c9e6faff5ba77748821862018
Alan Erives <alanerives@gmail.com>
2023-08-01 19:07:43 -0600

    grew and signup done

--- app/src/main/java/com/example/movefit21/signup.kt
+++ app/src/main/java/com/example/movefit21/signup.kt
@@ -1,11 +1,28 @@
 package com.example.movefit21
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+
+
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.signupv
+        val videoUri = Uri.parse(videoPath)
+
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+
+
+        videoView.setVideoURI(videoUri)
+
+        videoView.start()
     }
 }
